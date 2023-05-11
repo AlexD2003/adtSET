42cd6716a47546fe37de1acfe6e1fbc51ac12637
AlexD2003 <126864652+AlexD2003@users.noreply.github.com>
2023-05-11 07:27:15 +0300

    Add files via upload

---
+++ ADTset/Set.cpp
@@ -0,0 +1,109 @@
+﻿#include "Set.h"
+#include "SetITerator.h"
+
+Set::Set() {
+	//TODO - Implementation
+	//constructor
+	capacity = 10;
+	length = 0;
+	elements = new TElem[capacity];
+}//Θ(1)
+
+
+bool Set::add(TElem elem) {
+	//TODO - Implementation
+	int i = 0;
+	//initial poz=0
+	bool found = false;
+	//found bool
+	while (i <= length && found == false) {
+		if (elements[i] == elem)
+			//if an element is found,change found to true
+			found = true;
+		else
+			i++;
+	}
+	if (found == true) {
+		//if it is already present,ret false
+		return false;
+	}
+	else {
+		//resize formula
+		capacity *= 2;
+		TElem* newElements = new TElem[capacity];
+		for (i = 0; i <= length; i++) {
+			newElements[i] = elements[i];
+		}
+		elements = newElements;
+	}
+	//increase length and add elem
+	length++;
+	elements[length] = elem;
+	return true;
+}//O(length)
+
+
+bool Set::remove(TElem elem) {
+	
+	int pos = -1;
+	//search for elem in elements
+	for (int i = 0; i <= length; i++) {
+		if (elements[i] == elem) {
+			pos = i;
+			break;
+		}
+	}
+	//if the element is not in the array, return false
+	if (pos == -1) {
+		return false;
+	}
+	//shift the elements
+	for (int i = pos; i <= length - 1; i++) {
+		elements[i] = elements[i + 1];
+	}
+
+	//decrease the size of the array by 1
+	length--;
+	return true;
+
+}//O(length)
+
+bool Set::search(TElem elem) const {
+	//TODO - Implementation
+	int i;
+	//search for elem
+	for (i = 0; i <= length; i++) {
+		if (elements[i] == elem) {
+			return true;
+		}
+	}
+	return false;
+}//O(n)
+
+
+int Set::size() const {
+	//TODO - Implementation
+	return length;
+}//Θ(1)
+
+
+bool Set::isEmpty() const {
+	//TODO - Implementation
+	if (length == 0) {
+		return true;
+	}
+	return false;
+}//Θ(1)
+
+
+Set::~Set() {
+	//TODO - Implementation
+	delete[]elements;
+}//Θ(1)
+
+
+SetIterator Set::iterator() const {
+	return SetIterator(*this);
+}
+
+
---
+++ ADTset/SetIterator.cpp
@@ -0,0 +1,41 @@
+﻿#include "SetIterator.h"
+#include "Set.h"
+
+
+SetIterator::SetIterator(const Set& m) : set(m)
+{
+	//TODO - Implementation
+	currentPos = 0;
+}//Θ(1)
+
+
+void SetIterator::first() {
+	//TODO - Implementation
+	currentPos = 0;
+}//Θ(1)
+
+
+void SetIterator::next() {
+	//TODO - Implementation
+	currentPos++;
+}//Θ(1)
+
+
+TElem SetIterator::getCurrent()
+{
+	//TODO - Implementation
+	if (valid()) {
+		return set.elements[currentPos];
+	}
+	else {
+		return NULL_TELEM;
+	}
+}//Θ(1)
+
+bool SetIterator::valid() const {
+	//TODO - Implementation
+	return currentPos < set.size();
+}//Θ(1)
+
+
+
